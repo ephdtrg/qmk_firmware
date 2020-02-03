c42180b4e8d440d5d89eaea91c5c557dcf938c76
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-02-04 02:19:34 +0300

    create own custom keymap with RGB per layer switch

---
+++ keyboards/geekboards/tester/keymaps/deathridge/keymap.c
@@ -0,0 +1,42 @@
+#include QMK_KEYBOARD_H
+
+bool has_layer_changed = true;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(TG(1), LALT(KC_2), KC_PSCR, RGB_TOG, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT),
+	[1] = LAYOUT(KC_TRNS, KC_MUTE, KC_F14, KC_F15, KC_A, KC_S, KC_SCLN, KC_QUOT)
+};
+
+void matrix_init_user(void) {
+  //user initialization
+  rgblight_enable(); // enables Rgb, without saving settings
+  rgblight_sethsv(127, 255, 255); // sets the color to teal/cyan without saving
+  rgblight_mode(RGB_MATRIX_BREATHING); // sets mode to Fast breathing without saving
+}
+
+void matrix_scan_user(void) {
+  uint8_t layer = biton32(layer_state);
+  static uint8_t old_layer = 0;
+
+  if (old_layer != layer) {
+    has_layer_changed = true;
+    old_layer = layer;
+  }
+
+  if (has_layer_changed) {
+    has_layer_changed = false;
+
+    switch (layer) {
+      case 0:
+        rgblight_sethsv(127,255,255);
+      break;
+      case 1:
+        rgblight_sethsv(198,255,255);
+      break;
+    }
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
